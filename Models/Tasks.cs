8356ea8506ff7397468ae6c01cee1e7a93f4efb6
Keskil <kezoneko@gmail.com>
2020-01-26 11:50:18 +0900

    Работа с шаблоном

--- Models/Tasks.cs
+++ Models/Tasks.cs
@@ -9,16 +9,19 @@ namespace ToDo.Models
         public int ID { get; set; }
 
         // Поле заголовка задания
+        [Display(Name = "Заголовок")]
         public string Title { get; set; }
 
         // Поле даты и времени срока выполнения задания
-        [DataType(DataType.DateTime)]
+        [Display(Name = "Срок выполнения"), DataType(DataType.DateTime), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd, HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
 
         // Поле описания задания
+        [Display(Name = "Описание")]
         public string Description { get; set; }
 
         // Поле статуса выполнения задания
+        [Display(Name = "Статус"), UIHint("Status")]
         public bool Status { get; set; }
     }
 }
